b9e2add6e7501b0be6fe53a81c1b2ad29bcf4bc7
sarah hildt <sarahhildt@gmail.com>
2022-02-20 13:12:48 -0700

    database

---
+++ QRHunter/app/src/main/java/com/example/qrhunter/User.java
@@ -0,0 +1,18 @@
+package com.example.qrhunter;
+
+public class User {
+    private String username;
+    private String email;
+    public User(String username, String email){
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
