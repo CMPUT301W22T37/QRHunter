5752dcebd3241de8cbed99f70955876ddb27d89b
sharuhen <60045219+sharuhen@users.noreply.github.com>
2022-02-28 16:31:10 -0700

    Added the hash Score

--- QRHunter/app/src/main/java/com/example/qrhunter/QRCode.java
+++ QRHunter/app/src/main/java/com/example/qrhunter/QRCode.java
@@ -1,5 +1,7 @@
 package com.example.qrhunter;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.io.Serializable;
 import java.util.Random;
 
@@ -16,15 +18,36 @@ public class QRCode implements Serializable{
         return score;
     }
 
-    public int calculateScore(String string){
-        //Sharu
-
-        //Temp
-        Random rand = new Random();
-        return rand.nextInt(101);
+    public int calculateScore(String str){
+        String sha256hex = DigestUtils.sha256Hex(str);
+        int score=0;
+        for (int i = 0; i < str.length(); i++) {
+            // Counting occurrences of s[i]
+            int count = 1;
+            while (i + 1 < str.length() && str.charAt(i) == str.charAt(i + 1)) {
+                i++;
+                count++;
+            }
+            if(count>1) {
+                int a=Character.getNumericValue(sha256hex.charAt(i));
+//                System.out.println(Integer.parseInt(String.valueOf(sha256hex.charAt(i)),16)+ "=" + count + " ");
+                score=score+power(a,count-1);
+            }
+        }
+        return score;
     }
 
     public String getCode(){
         return code;
+    }
+
+    static int power(int base, int exponent) {
+        int power = 1;
+        //increment the value of i after each iteration until the condition becomes false
+        for (int i = 1; i <= exponent; i++)
+            //calculates power
+            power = power * base;
+        //returns power
+        return power;
     }
 }
