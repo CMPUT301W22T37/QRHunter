5c0ff0498245ccc97e9f48aab0e30896b2aff4a4
debyddog <ericscope@gmail.com>
2022-03-10 15:30:38 -0700

    Scanner Functionality
    QRScanner now creates a QRcode and passes it to the user

--- QRHunter/app/src/main/java/com/example/qrhunter/MainActivity.java
+++ QRHunter/app/src/main/java/com/example/qrhunter/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,ScanLoginCodeActivity.class));
+                startActivity(new Intent(MainActivity.this,ScanQRCodeActivity.class));
             }
         });
     }
--- QRHunter/app/src/main/java/com/example/qrhunter/ScanQRCodeActivity.java
+++ QRHunter/app/src/main/java/com/example/qrhunter/ScanQRCodeActivity.java
@@ -1,9 +1,6 @@
 package com.example.qrhunter;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,21 +10,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.Result;
 
 public class ScanQRCodeActivity extends AppCompatActivity {
     private CodeScanner mCodeScanner;
-    private User user;
-    private DataManagement dataManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        user = (User)intent.getSerializableExtra("User");
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        dataManager = new DataManagement(user,db);
         setContentView(R.layout.activity_scan_qr_code);
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
@@ -38,28 +28,6 @@ public class ScanQRCodeActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         Toast.makeText(ScanQRCodeActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
-
-                        final QRCode qrCode = new QRCode(result.getText());
-
-                        try{
-
-                            dataManager.addCode(qrCode, new CallBack() {
-                                @Override
-                                public void onCall(User user) {
-                                    Log.d("TAG", "Delete QR Code"+ qrCode.getCode());
-
-                                    Context context = getApplicationContext();
-
-                                    Intent intent =new Intent(context, MainMenu.class);
-                                    intent.putExtra("User",user);
-                                    startActivity(intent);
-                                }
-                            });
-
-                        } catch(Exception e){
-                            Log.d("TAG", "QR DNE");
-                        }
-
                     }
                 });
             }
