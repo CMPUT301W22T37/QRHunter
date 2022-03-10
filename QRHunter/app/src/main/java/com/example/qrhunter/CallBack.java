bf1be234ec62c3fb5eebeff764a950833533270f
debyddog <ericscope@gmail.com>
2022-03-10 15:33:07 -0700

    Revert "Merge branch 'main' into QR-Branch"
    This reverts commit 6d164db3e35b8b5387a3bbdeb76fdf41ad66eea2, reversing
    changes made to 5c0ff0498245ccc97e9f48aab0e30896b2aff4a4.

--- QRHunter/app/src/main/java/com/example/qrhunter/CallBack.java
+++
@@ -1,7 +0,0 @@
-package com.example.qrhunter;
-
-import java.util.ArrayList;
-
-public interface CallBack {
-    void onCall(User user);
-}
--- QRHunter/app/src/main/java/com/example/qrhunter/MainActivity.java
+++ QRHunter/app/src/main/java/com/example/qrhunter/MainActivity.java
@@ -14,23 +14,21 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db;
-    private Button createAccountBtn;
-    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        User testUser = new User("testUser1", "test1@gmail.com");
-//        HashMap<String, String> data = new HashMap<>();
-//        data.put(testUser.getUsername(), testUser.getEmail());
-//        db = FirebaseFirestore.getInstance();
-//        final CollectionReference collectionReference = db.collection("Users");
-//        collectionReference
-//                .document(testUser.getUsername())
-//                .set(data);
-        btn=(Button)findViewById(R.id.btnQRScanner);
+        User testUser = new User("testUser","test@gmail.com");
+        HashMap<String, String> data = new HashMap<>();
+        data.put(testUser.getUsername(), testUser.getEmail());
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("Users");
+        collectionReference
+                .document(testUser.getUsername())
+                .set(data);
+        Button btn=(Button)findViewById(R.id.btnQRScanner);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,12 +36,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,ScanQRCodeActivity.class));
             }
         });
-
-        createAccountBtn = findViewById(R.id.btnCreateAccount);
-    }
-
-    public void onCreateAccount(View view){
-        startActivity(new Intent(MainActivity.this,CreateAccount.class));
     }
 
 
