b2fd16f992f7f942380e937f2e5176dc6c46a4be
debyddog <ericscope@gmail.com>
2022-04-03 17:42:35 -0600

    Finished Owner Testing
    - Finished the OwnerActivity intent testing, however due to my emulator not running I'm unable to test currently

--- QRHunter/app/src/androidTest/java/com/example/qrhunter/OwnerPageTest.java
+++ QRHunter/app/src/androidTest/java/com/example/qrhunter/OwnerPageTest.java
@@ -1,10 +1,16 @@
 package com.example.qrhunter;
 
+import static org.junit.Assert.assertTrue;
+
+import android.view.View;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.robotium.solo.Solo;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,6 +21,7 @@ public class OwnerPageTest {
 
     private Solo solo;
     private String username;
+    private FloatingActionButton ownerButton;
 
 
 
@@ -26,14 +33,27 @@ public class OwnerPageTest {
         Random rand = new Random();
         int upperbound = 10000;
         int random_user = rand.nextInt(upperbound);
-        username = "TestUser"+random_user;
+        username = "TestOwner"+random_user;
         User user = new TestUser(username, username+"@gmail.com");
+        user.setOwner(true);
 
         solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
     }
 
     @Test
-    public void autoSignIn() throws InterruptedException {
-        solo.assertCurrentActivity("Wrong Activity",MainMenu.class);
+    public void OwnerTest() throws InterruptedException {
+        solo.clickOnView(solo.getView(R.id.owner_button));
+        solo.assertCurrentActivity("Wrong Activity",OwnerActivity.class);
+        solo.clickOnView(solo.getView(R.id.codes_button));
+        assertTrue(solo.getView(R.id.code_list_view).getVisibility() == View.VISIBLE);
+        assertTrue(solo.getView(R.id.user_list_view).getVisibility() == View.GONE);
+        solo.clickOnView(solo.getView(R.id.users_button));
+        assertTrue(solo.getView(R.id.user_list_view).getVisibility() == View.VISIBLE);
+        assertTrue(solo.getView(R.id.code_list_view).getVisibility() == View.GONE);
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
     }
 }
